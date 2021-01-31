bac881a4ef40b1061331482b4eb6ac5bf10813f0
awwmicky <michael_fred_alvarez@yahoo.com>
2021-01-30 18:43:09 -0800

    updated accordion

--- Javascript/accordion.js
+++ Javascript/accordion.js
@@ -1,24 +1,22 @@
 const accTitles = document.querySelectorAll('.accordion .acc-title');
 
+
 window.addEventListener('resize', (e) => {
   const accContent = document.querySelectorAll('.acc-content');
   accContent.forEach(content => {
     const accTitle = content.previousElementSibling;
     const isHidden = accTitle.classList.contains('acc-hide');
-    // const maxHeight =  content.style.maxHeight;
-    console.log('check', isHidden)
-    if (!isHidden) content.style.maxHeight = '100%';
+    const maxHeight =  content.style.maxHeight;
+    if (!isHidden && maxHeight !== '1000em') content.style.maxHeight = '1000em';
   })
 })
 
 accTitles.forEach(panel => {
-  const accContent = panel.nextElementSibling;
-  const height = accContent.scrollHeight;
-  accContent.style.maxHeight = `${height}px`;
-
   panel.addEventListener('click', (e) => {
     panel.classList.toggle('acc-hide')
     const isHidden = panel.classList.contains('acc-hide');
-    accContent.style.maxHeight = (isHidden) ? '0' : `${height}px`;
+    const accContent = panel.nextElementSibling;
+    const maxHeight = accContent.scrollHeight;
+    accContent.style.maxHeight = (isHidden) ? '0' : `${maxHeight}px`;
   })
 })
